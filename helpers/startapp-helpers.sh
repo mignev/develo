649b17563f489f800c8881de246c316595ac6586
Marian Ignev <m.ignev@gmail.com>
2015-03-01 11:13:20 +0200

    Add startapp helpers for mysql and postgresql

--- helpers/startapp-helpers.sh
+++ helpers/startapp-helpers.sh
@@ -1,40 +1,101 @@
+###
+# StartApp Cloud MongoDB Helper
+###
+
 function __develo_startapp_mongodb_helper {
   local APPNAME=$1
   local NAMESPACE=$2
 
-  local MONGODB_LOCAL_PORT=$3
-  local MONGODB_LOCAL_PORT=${MONGODB_LOCAL_PORT:=27017}
+  local LOCAL_PORT=$3
+  local LOCAL_PORT=${LOCAL_PORT:=27017}
 
-  local MONGO_CONNECTED=$(ps aux| grep "$APPNAME-.*$MONGODB_LOCAL_PORT"| grep -v grep)
+  local HELPER_NAME="MongoDB"
+  local ENV_VAR_DB_NAME="OPENSHIFT_MONGODB_DB_URL"
 
-  local MONGO_HOST=$(app ssh $APPNAME -n $NAMESPACE 'env|grep OPENSHIFT_MONGODB_DB_URL')
-  local MONGO_HOST=$(echo $MONGO_HOST| tr ' ' '\n'| grep OPENSHIFT_MONGODB_DB_URL=|sed 's/\///g'| sed 's/@/\:/g')
+  __develo_startapp_database_helper $APPNAME $NAMESPACE $LOCAL_PORT $HELPER_NAME $ENV_VAR_DB_NAME
+}
 
-  [[ "$MONGO_HOST" =~ ^OPENSHIFT_MONGODB_DB_URL=mongodb:(.*):(.*):(.*):(.*)$ ]] && \
-    local _MONGO_USER=${BASH_REMATCH[1]}
-    local _MONGO_PASS=${BASH_REMATCH[2]}
-    local _MONGO_REMOTE_HOST=${BASH_REMATCH[3]}
-    local _MONGO_REMOTE_PORT=${BASH_REMATCH[4]}
+###
+# StartApp Cloud MySQL Helper
+###
 
-  if [ -z "$MONGO_CONNECTED" ]; then
-    echo "Connecting to MongoDB on StartApp"
+function __develo_startapp_mysql_helper {
+  local APPNAME=$1
+  local NAMESPACE=$2
+
+  local LOCAL_PORT=$3
+  local LOCAL_PORT=${LOCAL_PORT:=3306}
+
+  local HELPER_NAME="MySQL"
+  local ENV_VAR_DB_NAME="OPENSHIFT_MYSQL_DB_URL"
+
+  __develo_startapp_database_helper $APPNAME $NAMESPACE $LOCAL_PORT $HELPER_NAME $ENV_VAR_DB_NAME
+}
+
+###
+# StartApp Cloud PostgreSQL Helper
+###
+
+function __develo_startapp_postgresql_helper {
+  local APPNAME=$1
+  local NAMESPACE=$2
+
+  local LOCAL_PORT=$3
+  local LOCAL_PORT=${LOCAL_PORT:=5432}
+
+  local HELPER_NAME="PostgreSQL"
+  local ENV_VAR_DB_NAME="OPENSHIFT_POSTGRESQL_DB_URL"
+
+  __develo_startapp_database_helper $APPNAME $NAMESPACE $LOCAL_PORT $HELPER_NAME $ENV_VAR_DB_NAME
+}
+
+
+function __develo_startapp_database_helper {
+
+  local APPNAME=$1
+  local NAMESPACE=$2
+
+  local LOCAL_PORT=$3
+  local LOCAL_PORT=${LOCAL_PORT:=27017}
+
+  local _HELPER_NAME=$4
+  local _ENV_DB_NAME=$5
+
+  local _HOST=$(app ssh $APPNAME -n $NAMESPACE "env|grep $_ENV_DB_NAME")
+  local _HOST=$(echo $_HOST| tr ' ' '\n'| grep $_ENV_DB_NAME=|sed 's/\///g'| sed 's/@/\:/g')
+
+  local _HELPER_LOWER_NAME=$(echo "$_HELPER_NAME"| tr '[:upper:]' '[:lower:]')
+  local _HELPER_UPPER_NAME=$(echo "$_HELPER_NAME"| tr '[:lower:]' '[:upper:]')
+
+  [[ "$_HOST" =~ ^${_ENV_DB_NAME}=${_HELPER_LOWER_NAME}:(.*):(.*):(.*):(.*)$ ]] && \
+    local _USER=${BASH_REMATCH[1]}
+    local _PASS=${BASH_REMATCH[2]}
+    local _REMOTE_HOST=${BASH_REMATCH[3]}
+    local _REMOTE_PORT=${BASH_REMATCH[4]}
+
+  local IS_CONNECTED=$(ps aux| grep "$APPNAME-.*$LOCAL_PORT"| grep -v grep)
+
+  if [ -z "$IS_CONNECTED" ]; then
+    echo "Connecting to $_HELPER_NAME on StartApp"
 
     local SSH_HOST=$(app show $APPNAME -n $NAMESPACE --gears ssh)
 
-    ssh $SSH_HOST -f -N -L $MONGODB_LOCAL_PORT:$_MONGO_REMOTE_HOST:$_MONGO_REMOTE_PORT
-    local MONGO_CONNECTED=$(ps aux| grep "$APPNAME-.*$MONGODB_LOCAL_PORT"| grep -v grep)
-    if [ -n "$MONGO_CONNECTED" ]; then
-      echo "MongoDB connected!"
+    ssh $SSH_HOST -f -N -L $LOCAL_PORT:$_REMOTE_HOST:$_REMOTE_PORT
+    local IS_CONNECTED=$(ps aux| grep "$APPNAME-.*$LOCAL_PORT"| grep -v grep)
+    if [ -n "$IS_CONNECTED" ]; then
+      echo "$_HELPER_NAME connected!"
+    else
+      echo "Uuups: There's a problem with $_HELPER_NAME connection. Cannot connect! :("
     fi
   else
-    echo "MongoDB is already connected!"
+    echo "$_HELPER_NAME is already connected!"
   fi
 
-  export STARTAPP_MONGODB_DATABASE=$APPNAME
-  export STARTAPP_MONGODB_USER=$_MONGO_USER
-  export STARTAPP_MONGODB_PASS=$_MONGO_PASS
-  export STARTAPP_MONGODB_HOST=127.0.0.1
-  export STARTAPP_MONGODB_PORT=$MONGODB_LOCAL_PORT
-  export STARTAPP_MONGODB_URI=127.0.0.1:$MONGODB_LOCAL_PORT
+  export STARTAPP_${_HELPER_UPPER_NAME}_DATABASE=$APPNAME
+  export STARTAPP_${_HELPER_UPPER_NAME}_USER=$_USER
+  export STARTAPP_${_HELPER_UPPER_NAME}_PASS=$_PASS
+  export STARTAPP_${_HELPER_UPPER_NAME}_HOST=127.0.0.1
+  export STARTAPP_${_HELPER_UPPER_NAME}_PORT=$LOCAL_PORT
+  export STARTAPP_${_HELPER_UPPER_NAME}_URI=127.0.0.1:$LOCAL_PORT
 
 }
